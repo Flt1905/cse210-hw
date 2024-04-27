4e16116a1ff851bf0e202aa21b8494ace5c838e4
Flt1905 <ftshilande@icloud.com>
2024-04-27 23:22:40 +0200

    prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade?");
+        string fromUser= Console.ReadLine();
+
+       int Grade = int.Parse(fromUser);
+
+       if (Grade >= 90)
+       {Console.WriteLine( "A");}
+       else if(Grade >= 80)
+       {Console.WriteLine( "B");}
+       else if(Grade >= 70)
+       {Console.WriteLine( "C");}
+       else if(Grade >= 60)
+       {Console.WriteLine( "D");}
+        else if(Grade < 60)
+        {Console.WriteLine( "F");}
+
+        
+        if (Grade >= 70)
+        {Console.WriteLine($"Congratulations ,you have passed the course.");}
+        else{Console.WriteLine($"You did not get a passing grade ,Please contact your mentor for more information.");}
+
     }
 }
