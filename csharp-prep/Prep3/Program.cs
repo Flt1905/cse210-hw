36173fd46178d6442e41b79d3c2ff0a439e2303c
Flt1905 <ftshilande@icloud.com>
2024-04-28 00:41:32 +0200

    prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Concurrent;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        
+
+       
+        
+        Console.WriteLine($"What is the magic number ? {number}");
+
+        int guessedNumber= -1;
+         while (number!= guessedNumber){
+        Console.WriteLine("What is your guess ?");
+       string inputFromUser= Console.ReadLine();
+        guessedNumber = int.Parse(inputFromUser);
+       
+       if (number < guessedNumber)
+        {Console.WriteLine("Lower");}
+       else if (number > guessedNumber) {Console.WriteLine("Higher");}
+         
+         else 
+            {Console.WriteLine("Congratulations, that is Correct");}
+         }
+
+    }     
 }
